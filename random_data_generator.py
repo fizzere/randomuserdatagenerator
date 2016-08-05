606bd85535e38edc1fea89c9ccac2236401c652f
Nikolai Vinogradov <Nikolai Vinogradov>
2016-08-05 20:03:10 +0300

    Added some unification for phone numbers and postcodes, and some functions for passport data and VATIN

--- random_data_generator.py
+++ random_data_generator.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+from random import randint
 
 URL = 'https://randomuser.me/api/'
 
@@ -8,27 +9,71 @@ class RandomDataGenerator(object):
     data = {}
 
     def __init__(self):
-        self.url = URL
         self.data = json.loads(urllib2.urlopen(URL).read())
 
-    def get_address(self):
-        street = self.data['results'][0]['location'][
+    def get_street(self):
+        street_data = self.data['results'][0]['location'][
             'street'].encode('ascii', 'ignore')
-        city = self.data['results'][0]['location'][
-            'city'].encode('ascii', 'ignore')
-        state = self.data['results'][0]['location'][
-            'state'].encode('ascii', 'ignore')
-        return '{0} ,{1}, {2}'.format(street, city, state)
+        street_name = street_data.split()[1:]
+        street = ' '.join(street_name)
+        return street
+
+    def get_home(self):
+        street_data = self.data['results'][0]['location'][
+            'street'].encode('ascii', 'ignore')
+        home_number = street_data.split()[0]
+        return home_number
+
+    def get_inn(self):
+        return randint(10000000000, 99999999999)
+
+    def get_passport_serial(self):
+        serial1 = randint(1, 99)
+        serial2 = randint(1, 99)
+        serial = '{0} {1}'.format(serial1, serial2)
+        return serial
+
+    def get_passport_number(self):
+        return randint(100000, 999999)
+
+    def get_passport_day(self):
+        return '{0}.{1}.'.format(randint(1, 31), randint(1, 12))
+
+    def get_passport_year(self):
+        return randint(1960, 2016)
+
+    def get_appartment(self):
+        return randint(1, 1000)
+
+    def get_city(self):
+        return self.data['results'][0]['location']['city'].encode('ascii', 'ignore')
+
+    def get_state(self):
+        return self.data['results'][0]['location']['state'].encode('ascii', 'ignore')
 
     def get_name(self):
         return '{0} {1}'.format(self.data['results'][0]['name']['first'].encode('ascii', 'ignore'),
                                 self.data['results'][0]['name']['last'].encode('ascii', 'ignore'))
 
     def get_postcode(self):
-        return self.data['results'][0]['location']['postcode']
+        postcode_raw = self.data['results'][0]['location']['postcode']
+        if isinstance(postcode_raw, int):
+            postcode = ''.join([x for x in str(postcode_raw).split()])
+        else:
+            postcode = ''.join([x for x in postcode_raw.split()])
+        if len(postcode) > 6:
+            postcode = postcode[:6]
+        elif len(postcode) < 6:
+            postcode = postcode + '1'
+        return postcode
 
     def get_phone(self):
-        return self.data['results'][0]['phone']
+        lst = ['(', ')', '-', ' ']
+        phone_raw = self.data['results'][0]['phone']
+        phone = ''.join([x for x in phone_raw if x not in lst])
+        if len(phone) > 10:
+            phone = phone[:10]
+        return phone
 
     def get_mail(self):
         return self.data['results'][0]['email'].encode('ascii', 'ignore')
